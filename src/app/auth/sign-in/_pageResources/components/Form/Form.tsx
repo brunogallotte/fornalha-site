c86fec8c346f6ae0df8d182cd670861c62f8b2a8
Bruno Gallotte <brunogallotte1@gmail.com>
2024-12-23 14:44:25 -0300

    Feat: add dark theme to layout

--- src/app/auth/sign-in/_pageResources/components/Form/Form.tsx
+++ src/app/auth/sign-in/_pageResources/components/Form/Form.tsx
@@ -1,12 +1,57 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const signInFormSchema = z.object({
+  email: z
+    .string({ required_error: "Digite seu email" })
+    .email("Email inválido"),
+  password: z
+    .string({ required_error: "Digite sua senha" })
+    .min(8, { message: "A senha deve ter pelo menos 8 caracteres" }),
+});
+
+type TFormSchema = z.infer<typeof signInFormSchema>;
 
 export const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TFormSchema>({ resolver: zodResolver(signInFormSchema) });
+
+  const onSubmit = (data: TFormSchema) => {
+    console.log(data);
+  };
+
   return (
-    <form className="flex flex-col gap-4">
-      <Input placeholder="Email" type="email" />
-      <Input placeholder="Password" type="password" />
-      <Button />
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col w-full max-w-xs gap-4"
+    >
+      <div className="flex flex-col gap-1">
+        <Input placeholder="Email" type="email" {...register("email")} />
+        {errors.email && (
+          <span className="text-red-700 text-sm">{errors.email.message}</span>
+        )}
+      </div>
+      <div className="flex flex-col gap-1">
+        <Input
+          placeholder="Password"
+          type="password"
+          {...register("password")}
+        />
+        {errors.password && (
+          <span className="text-red-700 text-sm">
+            {errors.password.message}
+          </span>
+        )}
+      </div>
+      <Button type="submit">Sign In</Button>
     </form>
   );
 };
--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -2,8 +2,14 @@ import { Form } from "./_pageResources/components/Form/Form";
 
 export default function SignIn() {
   return (
-    <section className="flex flex-col gap-6 w-full min-h-screen items-center justify-center">
-      <h1 className="text-2xl font-bold">Sign In</h1>
+    <section className="flex bg-background flex-col gap-6 w-full min-h-screen items-center justify-center">
+      <header className="flex flex-col gap-2 items-center">
+        <h1 className="text-2xl font-bold text-foreground">Sign In</h1>
+        <p className="text-muted-foreground max-w-xs text-center">
+          Use your credentials to access your account and start managing
+          fornalha dashboard.
+        </p>
+      </header>
 
       <Form />
     </section>
